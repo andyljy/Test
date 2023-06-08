536b332b3a62bfb98f0af914ef63c9169fb942b2
andyljy <123061889+andyljy@users.noreply.github.com>
2023-06-08 15:09:39 +0800

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -1,18 +1,21 @@
-﻿using System;
+// ﻿using System;
 
-namespace WebAppSSLManager.Models
-{
-    public class Constants
-    {
-        public const string AppPropertiesFileName = "appproperties.json";
-        public const string CertInfoFileName = "certinfo.json";
-        public const string AccountKeyFileName = "accountkey.pem";
-        public const string DefaultCertificatePassword = "C0mpl1c4t3d57r1ng";
-        public const string CertificateBlobContainer = "certificates";
-        public const string DefaultEmailSender = "AzureWebAppSSLManager@dbtek.com.hk";
-        public const string DefaultCA = "Let's Encrypt Authority";
-        public const string DefaultIntermediate = "R3";
-        public const int DefaultBatchSize = 0;
-        public const int DaysBeforeExpiryToRenew = 30;
-    }
-}
+// namespace WebAppSSLManager.Models
+// {
+//     public class Constants
+//     {
+//         public const string AppPropertiesFileName = "appproperties.json";
+//         public const string CertInfoFileName = "certinfo.json";
+//         public const string AccountKeyFileName = "accountkey.pem";
+//         public const string DefaultCertificatePassword = "C0mpl1c4t3d57r1ng";
+//         public const string CertificateBlobContainer = "certificates";
+//         public const string DefaultEmailSender = "AzureWebAppSSLManager@dbtek.com.hk";
+//         public const string DefaultCA = "Let's Encrypt Authority";
+//         public const string DefaultIntermediate = "R3";
+//         public const int DefaultBatchSize = 0;
+//         public const int DaysBeforeExpiryToRenew = 30;
+//     }
+// }
+
+
+public const string DefaultCertificatePassword = "C0mpl1c4t3d57r1ng";
