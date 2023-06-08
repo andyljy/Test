82625d97f84db4a227eb1d6ea152b4cf5459d83d
andyljy <123061889+andyljy@users.noreply.github.com>
2023-06-08 14:38:12 +0800

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WebAppSSLManager.Models
+{
+    public class Constants
+    {
+        public const string AppPropertiesFileName = "appproperties.json";
+        public const string CertInfoFileName = "certinfo.json";
+        public const string AccountKeyFileName = "accountkey.pem";
+        public const string DefaultCertificatePassword = "C0mpl1c4t3d57r1ng";
+        public const string CertificateBlobContainer = "certificates";
+        public const string DefaultEmailSender = "AzureWebAppSSLManager@dbtek.com.hk";
+        public const string DefaultCA = "Let's Encrypt Authority";
+        public const string DefaultIntermediate = "R3";
+        public const int DefaultBatchSize = 0;
+        public const int DaysBeforeExpiryToRenew = 30;
+    }
+}
